00f6ecd461e72142099f515c5f57a735e4432fb8
Xavi Abelaira Rueda <xavi_abelaira@outlook.com>
2019-10-12 16:21:56 +0200

    Implemented #: AppConfigBase created

---
+++ ChustaSoft.Tools.SecureConfig/AppConfigBase.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace ChustaSoft.Tools.SecureConfig
+{
+    public class AppConfigBase
+    {
+
+        public IDictionary<string, string> ConnectionStrings { get; set; }
+
+        public string this[string key] { get => ConnectionStrings[key]; }
+
+
+        public AppConfigBase() { }
+
+
+    }
+}
