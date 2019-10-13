ccb0df6037438837d0a3974c35069cde56b95298
Xavi Abelaira Rueda <xavi_abelaira@outlook.com>
2019-10-13 17:25:44 +0200

    Implemented #4: Extension methods for Configuration

---
+++ ChustaSoft.Tools.SecureConfig.UnitTests/ConfigurationExtensionsUnitTest.cs
@@ -0,0 +1,53 @@
+using ChustaSoft.Tools.SecureConfig.UnitTests.Helpers;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace ChustaSoft.Tools.SecureConfig.UnitTests
+{
+    public class ConfigurationExtensionsUnitTest
+    {
+        [Fact]
+        public void Given_Configuration_When_GetConnectionStrings_Then_DictionaryArrayRetrived()
+        {
+            var expectedConnections = 5;
+            var configuration = ConfigurationManagerUnitTestHelper.MockedConfiguration(5);
+
+            var result = configuration.GetConnectionStrings();
+
+            Assert.Equal(expectedConnections, result.Count);
+        }
+
+        [Fact]
+        public void Given_ConfigurationWithEmptyConnections_When_GetConnectionStrings_Then_DictionaryArrayRetrived()
+        {
+            var configuration = ConfigurationManagerUnitTestHelper.MockedConfiguration();
+
+            var result = configuration.GetConnectionStrings();
+
+            Assert.False(result.Any());
+        }
+
+        [Fact]
+        public void Given_NullConfiguration_When_GetConnectionStrings_Then_NullReferenceExceptionThrown()
+        {
+            IConfiguration configuration = null;
+
+            Assert.Throws<NullReferenceException>(() => configuration.GetConnectionStrings());
+        }
+
+        //Invalid Test, not possible to mock Get extension method from Microsoft.Extensions.Configuration.Binder
+        //[Fact]
+        //public void Given_ConfigurationAndType_When_GetSettings_Then_ObjectRetrived()
+        //{
+        //    var configuration = ConfigurationManagerUnitTestHelper.MockedConfiguration();
+
+        //    var result = configuration.GetSettings<TestConfig>();
+
+        //    Assert.Equal(result.TestInt, TestConstants.TEST_CONFIG_INT);
+        //    Assert.Equal(result.TestString, TestConstants.TEST_CONFIG_STRING);
+        //}
+
+    }
+}
---
+++ ChustaSoft.Tools.SecureConfig/ConfigurationExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+
+namespace ChustaSoft.Tools.SecureConfig
+{
+    public static class ConfigurationExtensions
+    {
+
+        private const string DEFAULT_CONNECTIONS_PARAM_NAME = "ConnectionStrings";
+        private const string DEFAULT_SETTINGS_PARAM_NAME = "AppSettings";
+
+
+        public static IDictionary<string, string> GetConnectionStrings(this IConfiguration configuration, string connectionsParamName = DEFAULT_CONNECTIONS_PARAM_NAME)
+        {
+            var connections = new Dictionary<string, string>();
+            var connectionStrings = configuration.GetSection(connectionsParamName).GetChildren();
+
+            foreach (var connection in connectionStrings)
+                connections.Add(connection.Key, connection.Value);
+
+            return connections;
+        }
+
+        public static TSettings GetSettings<TSettings>(this IConfiguration configuration, string settingsParamName = DEFAULT_SETTINGS_PARAM_NAME) 
+            where TSettings : AppSettingsBase
+        {
+            var settings = configuration.GetSection(settingsParamName).Get<TSettings>();
+
+            return settings;
+        }
+
+    }
+}
