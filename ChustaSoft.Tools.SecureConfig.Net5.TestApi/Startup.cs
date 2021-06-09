4944a9240eab5c7cfe5be142edb3963a80902584
Xavi Abelaira Rueda <xavi_abelaira@outlook.com>
2021-06-09 10:52:17 +0200

    Test projects created/updated for current supported frameworks

--- ChustaSoft.Tools.SecureConfig.Net5.TestApi/Startup.cs
+++ ChustaSoft.Tools.SecureConfig.Net5.TestApi/Startup.cs
@@ -4,8 +4,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
-namespace ChustaSoft.Tools.SecureConfig.NetCore3.TestApi
+namespace ChustaSoft.Tools.SecureConfig.Net5.TestApi
 {
     public class Startup
     {
@@ -18,22 +19,27 @@ namespace ChustaSoft.Tools.SecureConfig.NetCore3.TestApi
             _configuration = configuration;
         }
 
-
+        
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
 
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ChustaSoft.Tools.SecureConfig.Net5.TestApi", Version = "v1" });
+            });
 
             var testApikey = "TestPrivateKey-UseItInASecureWay.20200630";
             services.SetUpSecureConfig<AppSettings>(_configuration, testApikey);
         }
-
         
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ChustaSoft.Tools.SecureConfig.Net5.TestApi v1"));
             }
 
             app.UseHttpsRedirection();
